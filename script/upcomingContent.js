a990b2f00bec3bde003409810ac57ba581ebfd3c
Kien <trankie@oregonstate.edu>
2018-10-25 01:25:49 -0700

    Fixing graphical + functional bug around Upcoming Events. Delete will remove the right target and rows/columns are spaced dependent on input.

--- script/upcomingContent.js
+++ script/upcomingContent.js
@@ -1,3 +1,4 @@
+var eventNumber = 0;
 function delete_row(no)
 {
  document.getElementById("row"+no+"").outerHTML="";
@@ -9,17 +10,17 @@ function addUpcomingEvent(){
     var new_info=document.getElementById("upcomingEvent_Date_Time_Location").value;
        
     var table=document.getElementById("upcomingEventTable");
-    var table_len=(table.rows.length);
     if(new_name === '' || new_info === ''){
         alert("Error, please fill in both \n'Name of Upcoming Event' and 'Date/ Location/ Time'");
     }else{
         if(new_link === ''){
-            var row = table.insertRow(table_len).outerHTML="<tr class='UpERow' id='row"+table_len+"'><td align='center' id='name_row"+table_len+"' colspan='2' style='float:right' class='wrapword'>"+new_name+"</td><td id='info_col"+table_len+"'class='wrapword'><span style='border-left:2px solid #000;height:500px'></span>"
-                +new_info+"</td><td><input type='button' class='delete button buttonManipulate buttonDelete' onclick='delete_row("+table_len+")' style='float:right'></td></tr>";
+            var row = table.insertRow(-1).outerHTML="<tr class='UpERow' id='row"+eventNumber+"'><td align='center' id='name_row"+eventNumber+"' class='wrapword' style='border-right:2px solid #000;height:auto'>"+new_name+"</td><td align='center' id='info_col"+eventNumber+"'class='wrapword'>"
+                +new_info+"</td><td><input type='button' class='delete button buttonManipulate buttonDelete' onclick='delete_row("+eventNumber+")' style='float:right'></td></tr>";
         }else{
-            var row = table.insertRow(table_len).outerHTML="<tr class='UpERow' id='row"+table_len+"'><td align='center' id='name_row"+table_len+"' colspan='2' style='float:right' class='wrapword'><a class='center' target='_blank' rel='noopener noreferrer' href='"+new_link+"'>"+new_name+"</a></td><td id='info_col"+table_len+"'class='wrapword'><span style='border-left:2px solid #000;height:500px'></span>"
-                +new_info+"</td><td><input type='button' class='delete button buttonManipulate buttonDelete' onclick='delete_row("+table_len+")' style='float:right'></td></tr>";
+            var row = table.insertRow(-1).outerHTML="<tr class='UpERow' id='row"+eventNumber+"'><td align='center' id='name_row"+eventNumber+"' class='wrapword' style='border-right:2px solid #000;height:auto'><a class='center' target='_blank' rel='noopener noreferrer' href='"+new_link+"'>"+new_name+"</a></td><td align='center' id='info_col"+eventNumber+"'class='wrapword'>"
+                +new_info+"</td><td><input type='button' class='delete button buttonManipulate buttonDelete' onclick='delete_row("+eventNumber+")' style='float:right'></td></tr>";
         }
+        eventNumber++;
     }
     document.getElementById("upcomingEvent_Name").value="";
     document.getElementById("upcomingEvent_Link").value="";
